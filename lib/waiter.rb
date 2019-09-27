f55c784838c3fc90ff1365d8ec5010922bfe1b40
Kierra M <k.merriman32@hotmail.com>
2019-09-27 17:43:22 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -7,14 +7,23 @@ class Waiter
   @name = name
   @yrs_experience = yrs_experience
   @@all << self
-end
+ end
 
-def self.all 
+  def self.all 
   @@all 
-end
+  end
 
-def new_meal(customer, total, tip=0)
+  def new_meal(customer, total, tip=0)
     Meal.new(self, customer, total, tip)
   end
+  
+  def meals
+    Meal.all.select do |meal|
+    meal.waiter == self
+  end
+  end
+  
+    
+  
 
 end
